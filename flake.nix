cf908b7f31e7b97f862486eebe1a65fe60beda4a
john <muffinpost1@gmail.com>
2025-04-14 14:46:26 -0500

    try stuff

--- flake.nix
+++ flake.nix
@@ -1,19 +1,26 @@
 {
-  description = "A simple NixOS flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = { self, nixpkgs, home-manager, impermanence, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, home-manager, impermanence, ... }: {
     nixosConfigurations.john-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
+      specialArgs = { inherit inputs; };
+
       modules = [
         ./configuration.nix
 	impermanence.nixosModules.impermanence
 	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPkgs = true;
+	  home-manager.users.john = import ./home.nix;
+	}
       ];
     };
   };
