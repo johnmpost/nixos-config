dbfcfd1f45a0f74849b4ead04bcf8fb0119fd5a4
john <muffinpost1@gmail.com>
2025-04-14 01:00:26 -0500

    initial commit

---
+++ bashrc
@@ -0,0 +1,101 @@
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# have a big history size
+HISTSIZE=100000
+HISTFILESIZE=200000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# enable `**` for recursive glob matching
+shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# enable color support of ls
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+# aliases
+alias cpy="xclip -selection clipboard"
+alias pst="xclip -selection clipboard -o"
+alias dl="xrandr --output eDP --fb 1920x1080 --mode 1920x1080 --scale 1x1 --set TearFree on"
+alias dlu="xrandr --fb 2560x1080 --output eDP --mode 1920x1080 --scale-from 2560x1080 --output HDMI-A-0 --mode 2560x1080 --scale 1x1 --same-as eDP --set TearFree on --rate 74.99"
+alias c=clear
+alias ga="git add -A"
+alias gc="git commit -m"
+alias gp="git push"
+alias gs="git status"
+alias gl="git pull"
+alias gb="git branch"
+alias gco="git checkout"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gm="git merge"
+alias gbd="git branch -d"
+alias p="python3"
+alias l="ls -lh"
+alias la="ls -lah"
+alias hl=hledger
+alias nrbs="sudo nixos-rebuild switch"
+
+# custom commands
+maxbr () {
+  echo 255 | sudo tee /sys/class/backlight/amdgpu_bl0/brightness > /dev/null
+}
+
+gnc-txns () {
+  grep description \
+  | sed 's/<trn:description>//' \
+  | sed 's/<\/trn:description>//' \
+  | tac
+}
+
+pdf () {
+    firefox -new-window "$1"
+}
+
+combine_files() {
+    for file in "$@"; do
+        echo "File: $file"
+        cat "$file"
+        echo -e "\n"
+    done
+}
+
+csv() {
+  if [[ -f "$1" ]]; then
+    column -s, -t < "$1" | less -#2 -N -S
+  else
+    echo "File not found: $1"
+  fi
+}
+
+PS1="\[\]\[\e]0;\u: \w\a\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
+
+set -o vi
---
+++ configuration.nix
@@ -0,0 +1,157 @@
+{ config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+  impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+in
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      "${impermanence}/nixos.nix"
+      "${home-manager}/nixos"
+    ];
+
+  programs.fuse.userAllowOther = true;
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      "/etc/nixos"
+      "/var/log"
+      "/etc/NetworkManager/system-connections"
+    ];
+  };
+
+  users.mutableUsers = false;
+  users.users.john = {
+    isNormalUser = true;
+    hashedPassword = "$6$f18PZFZFnwIDW/ZS$n3A4zvkXM4wtmaL4Z8VFQIKdYgG.4plbT0cnl9cbK63ZW3d4b8RZ6rrPjpY33yuqQ46NwZip22EnEfUMrfsMq1";
+    extraGroups = [ "wheel" ];
+  };
+
+  home-manager.users.john = {
+    imports = [ "${impermanence}/home-manager.nix" ];
+    home.stateVersion = "24.11";
+
+    fonts.fontconfig.enable = true;
+
+    home.packages = with pkgs; [
+      vim
+      neovim
+      firefox
+      alacritty
+      git
+      (pkgs.nerdfonts.override { fonts = [ "Ubuntu" "UbuntuMono" ]; })
+      python314
+      neofetch
+      ripgrep
+      tree
+      scrot
+      ungoogled-chromium
+      gnucash
+      hledger
+      xclip
+      nodejs_23
+      keepassxc
+      ffmpeg_6
+      pulseaudio
+      pavucontrol
+      xss-lock
+      i3lock
+      lesspipe
+      alacritty
+      kitty
+      gnumake
+      libgcc
+      pkg-config
+      (pkgs.st.override { conf = builtins.readFile /etc/nixos/st-config.h;
+	patches = [
+	  (fetchpatch {
+	    url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.9.2.diff";
+            sha256 = "0ymc5db75cwmdvv8ak3bfaf7iz4snj65fbmhrl9blv7h7pw3pdld";
+	  })
+	  # (fetchpatch {
+	    # url = "https://st.suckless.org/patches/columnredraw/st-columnredraw-20241119-fb8569b.diff";
+            # sha256 = "1243mrpi06ldr8d7b554slhpaf02j7j4cpzrnfqanfms0mhijix5";
+	  # })
+	  # (fetchpatch {
+	    # url = "https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.5.diff";
+            # sha256 = "108h30073yb8nm9x04x7p39di8syb8f8k386iyy2mdnfdxh54r04";
+	  # })
+	];
+      })
+    ];
+
+    programs = {
+      home-manager.enable = true;
+    };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+      };
+    };
+
+    home.file.".config/i3/config".source = /etc/nixos/i3-config;
+    home.file.".config/i3/status-config".source = /etc/nixos/i3-status-config;
+    home.file.".bashrc".source = /etc/nixos/bashrc;
+    home.file.".xinitrc".source = /etc/nixos/xinitrc;
+    home.file.".gitconfig".source = /etc/nixos/gitconfig;
+    home.file.".config/alacritty/alacritty.toml".source = /etc/nixos/alacritty.toml;
+
+    home.persistence."/nix/persist/home/john" = {
+      directories = [
+        ".mozilla"
+        ".ssh"
+        "dl"
+      ];
+      files = [
+        ".bash_history"
+      ];
+      allowOther = true;
+    };
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "john-nixos";
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Chicago";
+  services.xserver.enable = true;
+  services.xserver.autorun = false;
+  services.xserver.displayManager.startx.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.libinput.touchpad.naturalScrolling = true;
+  services.libinput.enable = true;
+  services.openssh.enable = true;
+  networking.firewall.enable = false;
+  services.xserver.deviceSection = ''Option "TearFree" "true"'';
+  services.envfs.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    # (st.overrideAttrs (oldAttrs: rec {
+      # buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
+      # patches = [
+        # (fetchpatch {
+          # url = "https://st.suckless.org/patches/clickurl/st-clickurl-0.8.5.diff";
+          # sha256 = "1a97q2hv63j2nq6vw924a6si8wg4903rbkrmml31zlhjgl9nbmq4";
+        # })
+      # ];
+      # configFile = writeText "config.def.h" (builtins.readFile /etc/nixos/st-config.h);
+      # postPatch = oldAttrs.postPatch ++ ''cp ${configFile} config.def.h'';
+    # }))
+  ];
+
+  system.stateVersion = "24.11"; # never change
+}
+
