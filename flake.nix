ac254889a9953c5a2b0437f00ee653b66a3c05b1
john <muffinpost1@gmail.com>
2025-04-14 19:12:24 -0500

    fix typo

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 	{
 	  imports = [ home-manager.nixosModules.home-manager ];
 	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPkgs = true;
+	  home-manager.useUserPackages = true;
 	  home-manager.users.john = { ... }: {
 	    imports = [
 	      impermanence.homeManagerModules.impermanence
