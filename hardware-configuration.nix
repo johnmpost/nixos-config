2543cca39af98a02d7c02858709bc59e2cb5c8d0
John Post <john.post@useworkshop.com>
2026-09-17 03:59:00 +0000

    try remove amdgpu from boot

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -9,7 +9,6 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
